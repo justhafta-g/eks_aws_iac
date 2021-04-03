deabb3c325519c84521faeb5b54cad6cda6f7987
Oleksandr Kyktenko <oleksandr.kyktenko@synapse.com>
2021-04-03 13:47:06 +0300

    Added route-table-association.tf

--- routing-tables.tf
+++ routing-tables.tf
@@ -3,10 +3,42 @@ resource "aws_route_table" "public" {
 
   route {
     cidr_block = "0.0.0.0/0"
+
+    # Identifier of VPC IGW or Virtual Private Gateway
     gateway_id = aws_internet_gateway.main.id
   }
 
   tags = {
     Name = "public"
+  }
+}
+
+resource "aws_route_table" "private1" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+
+    # Identifier of VPC IGW or Virtual Private Gateway
+    gateway_id = aws_nat_gateway.gw1.id
+  }
+
+  tags = {
+    Name = "private1"
+  }
+}
+
+resource "aws_route_table" "private2" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+
+    # Identifier of VPC IGW or Virtual Private Gateway
+    gateway_id = aws_nat_gateway.gw2.id
+  }
+
+  tags = {
+    Name = "private2"
   }
 }
